c4408b0210addd7e76a4545a52060e289d33ba2c
linh <phmalinha@gmail.com>
2026-06-06 14:59:36 +0800

    database

--- parking_control/database/seeders/AdminUserSeeder.php
+++ parking_control/database/seeders/AdminUserSeeder.php
@@ -14,11 +14,18 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-       User::create([
-        'name' => 'Admin',
-        'email' => 'admin@gmail.com',
-        'password' => Hash::make('123456'),
-    ]);
+    //    User::create([
+    //     'name' => 'Admin',
+    //     'email' => 'admin@gmail.com',
+    //     'password' => Hash::make('123456'),
+    // ]);
+        if (\App\Models\User::where('email', 'admin@gmail.com')->count() == 0) {
+            \App\Models\User::create([
+                'name' => 'Admin Parking',
+                'email' => 'admin@gmail.com',
+                'password' => \Illuminate\Support\Facades\Hash::make('12345678'),
+            ]);
+        }
 
     }
 }
